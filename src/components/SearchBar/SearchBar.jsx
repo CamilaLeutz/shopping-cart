f46b0338c6c0f887d5d6a305ed3db9a2e9094dc5
camilaleutz <leutz1989@gmail.com>
2023-10-02 16:12:43 -0300

    antes do context api, searchbar funcionando

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,15 +2,22 @@ import React, { useState } from "react"
 import { BsSearchHeart } from "react-icons/bs"
 
 import "./SearchBar.css"
+import fetchProducts from "../../api/FetchProducts"
 
 function SearchBar(){
 
   const [searchValue, setSearchValue] = useState("")
 
-
+  const handleSearch = async (event) => {
+    event.preventDefault()
+   
+    const products = await fetchProducts(searchValue)
+    console.log(products)
+    setSearchValue("")
+  }
 
   return (
-    <form className="search-bar" >
+    <form className="search-bar" onSubmit={handleSearch} >
       <input type="search" 
         value={searchValue}
         placeholder="Buscar produtos" 
